5a5c4a8178ae65d76a94ddbe2d2912952f9042a2
camsitosutrito <jos.quezadar@duocuc.cl>
2024-06-07 11:33:16 -0400

    avanze por la cara

---
+++ sepe.py
@@ -0,0 +1,23 @@
+import os
+import time
+import msvcrt
+import csv
+def nombre_mas_largo(p_n):
+    for x in range(3):
+        if x==0:
+            nombre_largo = p_n[0]
+    else:
+        if len(p_n[x]) > len(nombre_largo):
+            nombre_largo = p_n[x]
+    print("El nombre más largo es:",nombre_largo)
+
+nombresitos=[]
+
+with open('nombresitos.csv','w',newline='') as archivo:
+    escritor = csv.writer(archivo)
+    for ciclo in range(3):
+        nombresito=input("Ingrese nombre: ")
+        nombresitos.append(nombresito)
+    escritor.writerow(nombresitos)
+
+nombre_mas_largo(nombresitos)
